d2cdee5f0904e78e686edc8f781c4003cd4ec0b6
Porson <porsonlee@gmail.com>
2020-03-09 01:11:43 -0700

    added text submit handler and styled it

--- src/App.js
+++ src/App.js
@@ -10,6 +10,22 @@ function Todo({ todo, index }) {
   )
 }
 
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState("");
+
+const handleSubmit = e => {
+  e.preventDefault();
+  if(!value) return;
+  addTodo(value);
+  setValue("")
+}
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+    </form>
+  )
+}
 
 function App() {
   const [todos, setTodos] = useState([
@@ -27,12 +43,18 @@ function App() {
     }
   ]); 
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
           <Todo key={index} index={index} todo={todo} />
         ))}
+        <TodoForm addTodo={addTodo} />
       </div>
     </div>
   )
