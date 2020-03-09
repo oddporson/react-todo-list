cc52d542a71abbb4f80b1ee1de5265d889bcbabf
Porson <porsonlee@gmail.com>
2020-03-09 01:18:24 -0700

    implemented complete button that cross the task off

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from "react"
 import "./App.css";
 
 
-function Todo({ todo, index }) {
+function Todo({ todo, index, completeTodo }) {
   return (
-    <div className="todo">
+    <div style={{textDecoration: todo.isCompleted ? 'line-through' : ''}} className="todo">
       { todo.text }
+      <div>
+        <button onClick={()=> completeTodo(index)}>Complete</button>
+      </div>
     </div>
   )
 }
@@ -22,7 +25,12 @@ const handleSubmit = e => {
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" className="input" value={value} onChange={e => setValue(e.target.value)} />
+      <input 
+        type="text" 
+        className="input" 
+        value={value}
+        placeholder="Add Todo..." 
+        onChange={e => setValue(e.target.value)} />
     </form>
   )
 }
@@ -48,11 +56,17 @@ function App() {
     setTodos(newTodos);
   }
 
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = true;
+    setTodos(newTodos);
+  }
+
   return (
     <div className="app">
       <div className="todo-list">
         {todos.map((todo, index) => (
-          <Todo key={index} index={index} todo={todo} />
+          <Todo key={index} index={index} todo={todo} completeTodo={completeTodo} />
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
